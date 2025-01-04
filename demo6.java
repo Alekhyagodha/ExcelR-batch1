955cca03273721945c20dc49606249b1318e4f22
Alekhyagodha <godhaalekhya30@gmail.com>
2025-01-04 12:30:34 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,11 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String username = "Alekhya";
+        String pwd = "Alekhya01";
+        while ((username == "Alekhya") && (pwd == "Alekhya01")) {
+              System.out.println("hello Alekhya welcome to mallareddyuniversity");
+      break;
+    }
+    
+}
+}
